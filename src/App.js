bdfb0bf75955e04878adda12a198461b01fa1f87
LagAntunes <lag151617@gmail.com>
2022-04-06 18:22:03 -0300

    adicionando arquivos e imagens

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import './App.css';
-import Botao from './components/botao.js';
 
 function App() {
-  const mano = <h2>Aprendendo o corre, mano</h2>
-
-  function teste() {
-    return(
-      <div>
-      <p>aaaaaaaaaaaaaaaaaa</p>
-      </div>
-    )
-  }
-
   return (
     <div className="App">
-      <h1 className="classeTeste">Iniciando React</h1>
-      <p>Testando a const: {mano}</p>
-      {teste()}
-
-      <Botao/>
+      <h1>Iniciando React</h1>
     </div>
   );
 }
