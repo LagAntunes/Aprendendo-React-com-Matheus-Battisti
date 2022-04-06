b7d24bb6f03d9fe394270146bcea323859770db3
LagAntunes <lag151617@gmail.com>
2022-04-06 18:55:17 -0300

    adicionando rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Iniciando React</h1>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newProject">Novo Projeto</Link>
+      </div>
+
+      <Routes>
+        <Route path="/"> /**** Parei na aula 18 */
+          <Home />
+        </Route>
+
+        <Route path="/company">
+          <Company />
+        </Route>
+
+        <Route path="/contact">
+          <Contact />
+        </Route>
+
+        <Route path="/newProject">
+          <NewProject />
+        </Route>
+      </Routes>
+
+      <p>Footer</p>
+    </Router>
   );
 }
 
