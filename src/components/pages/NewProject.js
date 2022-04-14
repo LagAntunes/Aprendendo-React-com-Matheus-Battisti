63db855a8edd1372d871768d10518efbe5e47159
LagAntunes <lag151617@gmail.com>
2022-04-13 21:38:04 -0300

    adicionando componentes no formulário

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,8 +1,14 @@
+import styles from "./NewProject.module.css"
+
+import ProjectForm from "../Project/ProjectForm";
+
 function NewProject(){
     return(
-        <>
-            <h1>NewProject</h1>
-        </>
+        <div className={styles.newProjectContainer}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm/>
+        </div>
     )
 }
 
